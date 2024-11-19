3b99107afa4c88d5c2d5c071d15a74fb6a94cf09
mattbtyler <mattbtyler@gmail.com>
2024-11-19 13:03:20 -0600

    Adding mysql docker resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,22 +6,22 @@ import { AppService } from './app.service'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'mydb.sqlite',
-      entities: [Video],
-      synchronize: true // Only for development
-    }),
     // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'root',
-    //   database: 'videos',
-    //   entities: [],
-    //   synchronize: true
+    //   type: 'sqlite',
+    //   database: 'mydb.sqlite',
+    //   entities: [Video],
+    //   synchronize: true // Only for development
     // }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'video-library',
+      entities: [Video],
+      synchronize: true
+    }),
     TypeOrmModule.forFeature([Video])
   ],
   controllers: [AppController],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  // Enable CORS
+  // Could move this origin to an environment variable
+  app.enableCors({
+    origin: 'http://localhost:5173', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true
+  })
   await app.listen(3000)
 }
 bootstrap()
